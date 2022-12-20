5bd4abe5717691ec0244ad2aa1052c6568c9e1b3
krishna-bansall <bansalkrishna321go@gmail.com>
2022-12-20 16:12:07 +0530

    Some Edits

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print("Hello World")
+from decouple import config
+import openai
+openai.api_key = config("API_KEY")
+
+
+user_prompt = input("Enter what would you like to Create Today? ")
+
+output = openai.Completion.create(
+  model="text-curie-001",
+  prompt = user_prompt,
+  max_tokens=1000,
+  temperature=0.3
+)
+print('\n')
+print(output.choices[0].text)
